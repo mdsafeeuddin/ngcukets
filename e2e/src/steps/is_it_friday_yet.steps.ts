98c807548ade25970ec0a0486eafb2d5cc7b7b8d
safeeuddin <ba@maainfotek.com>
2019-02-11 19:13:32 +0530

    Configured cucumber with ts

---
+++ e2e/src/steps/is_it_friday_yet.steps.ts
@@ -0,0 +1,26 @@
+import { defineSupportCode } from 'cucumber';
+import { assert } from 'chai';
+
+defineSupportCode(({Given, When, Then}) => {
+
+    function isItFriday(today) {
+        if (today === "Friday") {
+          return "TGIF";
+        } else {
+          return "Nope";
+        }
+      }
+      
+      
+      Given('today is {string}', function (givenDay) {
+        this.today = givenDay;
+      });
+      
+      When('I ask whether it\'s Friday yet', function () {
+        this.actualAnswer = isItFriday(this.today);
+      });
+      
+      Then('I should be told {string}', function (expectedAnswer) {
+        assert.equal(this.actualAnswer, expectedAnswer);
+      });
+})
